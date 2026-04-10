0b17efd7a93e3cf601b1962cacb2f1946936c8e1
binarytechstudio <binarytechstudio@protonmail.com>
2026-04-11 01:03:43 +0200

    update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -8,12 +8,10 @@ const branch =
   "main";
 
 export default defineConfig({
+  // Jeśli jesteśmy na produkcji, używamy ID, jeśli lokalnie - niekoniecznie
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID || "57ca8630-b466-4f01-85a4-4f7ec5132600",
+  token: process.env.TINA_TOKEN,
   branch: "main",
-
-  // Get this from tina.io
-  clientId: "57ca8630-b466-4f01-85a4-4f7ec5132600",
-  // Get this from tina.io
-  token: "7e9af7768c292998f8a610a962ff9fe996a1a65a",
 
   build: {
     outputFolder: "admin",
